075d85b748dbb212aa4fabf322eaec5c60295ac5
mitchel <edahmitchel@example.com>
2023-02-05 21:14:28 +0100

    connected mongo database and i cross env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      //  make config accessible to all modules
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }), MongooseModule.forRoot(process.env.MONGO_URI)
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
